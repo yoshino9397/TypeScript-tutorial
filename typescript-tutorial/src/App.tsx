6c90fc441af01a61cf31f8992128925d9d1acd5f
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-02-23 02:55:44 -0800

    change json

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -1,64 +1,53 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-let name:string ;
-let age:number;
-let isStudent:boolean;
-let hobbies:string[];
-let role:[number,string];
+let name: string;
+let age: number;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
 
-
-function printName(name:string){
+function printName(name: string) {
   console.log(name);
 }
-printName('yoshino')
+printName("yoshino");
 
-
-let printName2:(name:string) =>never;
-let personName:unknown;
+let printName2: (name: string) => never;
+let personName: unknown;
 ///It's better to use 'never' and 'unknown' than using 'void,any'.
-
 
 // type Person={
 //   name:string
 //   age?:number
 // }
-interface Person{
-  name:string
-  age?:number
+interface Person {
+  name: string;
+  age?: number;
 }
 //'type' and 'interface' are almost same!
 //but better to use 'interface',because⏬
-interface Guy extends Person{
-  profession:string
+interface Guy extends Person {
+  profession: string;
 }
 
-
-let person:Person={
-  name:"yoshino",
-  age:25
+let person: Person = {
+  name: "yoshino",
+  age: 25,
 };
 
-let lotOfPerson:Person[];
+let lotOfPerson: Person[];
 
-
-type X={
-  a:string;
-  b:number;
-}
-type Y=X&{
-  c:string;
-  d:number;
-}
-
-
+type X = {
+  a: string;
+  b: number;
+};
+type Y = X & {
+  c: string;
+  d: number;
+};
 
 function App() {
-  return (
-    <div className="App">
-      Hello World App
-    </div>
-  );
+  return <div className="App">Hello World App</div>;
 }
 
 export default App;
--- typescript-tutorial/src/AppReact.tsx
+++ typescript-tutorial/src/AppReact.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import './AppReact.css';
-import InputField from './components/InputField'
+import React, { useState } from "react";
+import "./AppReact.css";
+import InputField from "./components/InputField";
 import { Todo } from "./model";
 import TodoList from "./components/TodoList";
 
 const AppReact: React.FC = () => {
-  const [todo, setTodo] = useState<string>('');
+  const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const handleAdd = (e: React.FormEvent) => {
@@ -13,21 +13,17 @@ const AppReact: React.FC = () => {
 
     if (todo) {
       setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
-      setTodo('')
+      setTodo("");
     }
-  }
-
+  };
 
   return (
     <div className="AppReact">
-      <span className="heading">
-        Todo List
-      </span>
+      <span className="heading">Todo List</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
       <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
-}
-
+};
 
 export default AppReact;
